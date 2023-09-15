880359c5c76978a88c8baf0e574877fcc5b1d666
Harry Smith <harry.smith@endava.com>
2023-09-15 15:51:48 +0100

    Initial commit of backstage templates

---
+++ gce/main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+  project = "${{ values.projectId }}"
+  region  = "${{ values.deploymentRegion }}"
+  zone    = "${{ values.deploymentZone }}"
+}
+
+resource "google_compute_instance" "backstage_vm" {
+  provider = google
+  name = "${{ values.instanceName }}"
+  machine_type = "${{ values.instanceMachineType }}"
+  zone = "${{ values.deploymentZone }}"
+
+  labels = {
+    environment = "${{ values.deploymentEnvironment }}"
+  }
+
+  boot_disk {
+    source = "${google_compute_disk.backstage_vm_compute_disk.name}"
+  }
+
+  network_interface {
+    network = "default"
+  }
+  # Some changes require full VM restarts
+  # consider disabling this flag in production
+  #   depending on your needs
+  allow_stopping_for_update = true
+}
+
+resource "google_compute_disk" "backstage_vm_compute_disk" {
+  provider = google
+  name = "backstage-vm-bootdisk"
+  type = "pd-ssd"
+  zone = "${{ values.deploymentZone }}"
+  image = "${{ values.instanceDiskOS }}"
+  labels = {
+    environment = "${{ values.deploymentEnvironment }}"
+  }
+  size = "${{ values.instanceDiskSize }}"
+}
